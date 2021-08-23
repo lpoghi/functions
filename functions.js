28028ad2ec95b10501388f9cfc828606e6f59303
lpoghi <85479967+lpoghi@users.noreply.github.com>
2021-08-23 11:41:58 +0200

    Add files via upload

---
+++ functions.js
@@ -0,0 +1,23 @@
+const makePizza = function () {
+    console.log('take ready-made dough and put it on a baking tray');
+    console.log('spread tomato sauce on the dough');
+    console.log('put cheese over de tomato sauce');
+    console.log('add chosen condiments');
+    console.log('bake in oven');
+
+};
+
+
+
+const makeSushi = function () {
+    console.log('boil rice');
+    console.log('put rice ond rolling mat with nori');
+    console.log('add condiments');
+    console.log('roll');
+};
+
+makeSushi();
+makeSushi();
+makeSushi();
+makePizza();
+makePizza();
